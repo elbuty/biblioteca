aff560e16db7f191defe26443a8eb039abec0fa2
elbuty <elbuty@gmail.com>
2018-05-18 14:51:07 +0200

    viernes de resurreccion

--- ayuda.php
+++ ayuda.php
@@ -10,56 +10,13 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+
 </head>
 <body>
-<header>
-  <div class="jumbotron jumbotron-fluid jumbo-encabezado">
-    <div class="container">
-        <h1 class="display-4">Biblioteca Virtual</h1>
-        <p class="lead">Ayuda</p>
-    </div>
-  </div>
-</header>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="ayuda.php">Ayuda</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="indexPrincipal.php">Inicio<span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Usuario</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Ver</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Registrar
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="indexEditorial.php">Editorial</a>
-          <a class="dropdown-item" href="indexLibro.php">Libro</a>
-          <a class="dropdown-item" href="indexSocios.php">Socio</a>
-        </div>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="enlaceBarraNavegacion" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Ver
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="leerEditorial.php">Editorial</a>
-          <a class="dropdown-item" href="leerLibro.php">Libro</a>
-          <a class="dropdown-item" href="leerSocio.php">Socio</a>
-        </div>
-      </li>
-    </ul>
-  </div>
-  </nav>
-  <p>Lorem ipsum dolor sit amet consectetur adipiscing elit congue enim, suscipit commodo curabitur sodales tempus condimentum dui nunc mi, aenean facilisi ornare augue nulla orci mus vulputate. Tempor cras egestas a feugiat sodales gravida proin, duis litora vulputate torquent aliquet. Magna nulla egestas lobortis turpis nisi aenean tellus eleifend sem, maecenas gravida litora curae congue nascetur volutpat. Dignissim enim pellentesque sapien praesent fermentum ad dui, gravida felis volutpat netus porta potenti ultricies, nostra duis nam cras nascetur varius. Vestibulum elementum est nunc erat torquent dictum venenatis in bibendum, hac at risus pulvinar vulputate facilisi magnis dictumst pellentesque, ultricies sem tortor class porta velit nascetur nam. Pellentesque platea est dignissim per nec suscipit faucibus nisi, ultrices rutrum elementum mi vel curae ad, dis posuere sociis egestas sagittis non aptent. Curabitur lectus tincidunt primis lobortis cum eleifend vehicula dictum himenaeos, natoque dis mattis dapibus montes massa pulvinar risus porttitor, semper praesent aptent phasellus dignissim sapien ac fusce. Sollicitudin donec dictum tortor sodales imperdiet magnis curae, taciti leo cras a aptent suscipit lacinia sagittis, et molestie potenti eros varius luctus. Lacus purus sodales pulvinar sapien eleifend nullam, tellus litora dictum vulputate ornare, sagittis diam tortor torquent lobortis.
+<?php
+    include("body.php");
+ ?>
+  <p style="color:red";>Lorem ipsum dolor sit amet consectetur adipiscing elit congue enim, suscipit commodo curabitur sodales tempus condimentum dui nunc mi, aenean facilisi ornare augue nulla orci mus vulputate. Tempor cras egestas a feugiat sodales gravida proin, duis litora vulputate torquent aliquet. Magna nulla egestas lobortis turpis nisi aenean tellus eleifend sem, maecenas gravida litora curae congue nascetur volutpat. Dignissim enim pellentesque sapien praesent fermentum ad dui, gravida felis volutpat netus porta potenti ultricies, nostra duis nam cras nascetur varius. Vestibulum elementum est nunc erat torquent dictum venenatis in bibendum, hac at risus pulvinar vulputate facilisi magnis dictumst pellentesque, ultricies sem tortor class porta velit nascetur nam. Pellentesque platea est dignissim per nec suscipit faucibus nisi, ultrices rutrum elementum mi vel curae ad, dis posuere sociis egestas sagittis non aptent. Curabitur lectus tincidunt primis lobortis cum eleifend vehicula dictum himenaeos, natoque dis mattis dapibus montes massa pulvinar risus porttitor, semper praesent aptent phasellus dignissim sapien ac fusce. Sollicitudin donec dictum tortor sodales imperdiet magnis curae, taciti leo cras a aptent suscipit lacinia sagittis, et molestie potenti eros varius luctus. Lacus purus sodales pulvinar sapien eleifend nullam, tellus litora dictum vulputate ornare, sagittis diam tortor torquent lobortis.
 
 Ultrices rutrum ut vulputate nam lacus arcu torquent purus, id congue cras bibendum ridiculus interdum rhoncus vivamus eleifend, potenti dictumst netus aenean et leo consequat. Fusce dui conubia eleifend mus ligula etiam pellentesque enim facilisi tempor taciti tincidunt posuere a vulputate rhoncus tempus sociis, eget vel quisque id libero congue habitasse euismod semper fames gravida ultricies hendrerit viverra cum senectus aliquet. Nullam primis etiam metus mus lacinia conubia cras pharetra aenean, sagittis placerat cursus curabitur fringilla suscipit sed class proin leo, erat semper nam consequat et facilisi scelerisque nibh. Odio integer volutpat nullam erat dis non magnis magna nascetur porta sociosqu proin, elementum id faucibus dictum nostra ac nibh morbi ullamcorper sociis dignissim laoreet, donec porttitor hendrerit maecenas habitasse felis litora vulputate arcu est ligula. Lectus felis eleifend nunc elementum ante semper arcu, hac maecenas sed potenti diam aliquet nisi turpis, eget aenean purus erat sodales urna. Ultrices justo eleifend penatibus habitant potenti fermentum sapien aptent, duis nam bibendum inceptos orci purus quam libero laoreet, viverra eu suspendisse pharetra facilisis eget euismod vel, ridiculus placerat mollis eros suscipit metus ac. Lectus natoque tempor cras viverra risus curae phasellus cubilia, ornare leo tempus vehicula nisi mus molestie fames, non dictum a nunc ullamcorper egestas inceptos tellus, habitant dignissim parturient duis blandit elementum suscipit. Morbi primis curae phasellus pulvinar tempus ullamcorper penatibus auctor, facilisis duis pretium ultricies urna elementum ultrices leo, litora ridiculus praesent inceptos cursus libero nascetur.
 
--- body.php
+++ body.php
@@ -1,9 +1,14 @@
+<style media="screen">
+body{
+ background:url("img/letras.jpg");
+}
+.opacity{
+ background-color:rgba(255,0,0,0.2);
+ width:200px;
+ height:70px;
+}
+</style>
 <body >
 <?php include("header.php"); ?>
-<script>
-    $(document).ready(function() {
-      $("body").css("background-image","url('img/books-1035553.jpg')");
-    });
 
-</script>
 </body>
